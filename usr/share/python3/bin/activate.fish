1767273e6953f743aeb5cc68080edfd2d771d9ae
lifesboy <lifesboy@yahoo.com>
2022-02-12 07:36:15 +0700

    Migrate to python3 env execution

--- usr/share/python3/bin/activate.fish
+++ usr/share/python3/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/usr/share/python/scirius"
+set -gx VIRTUAL_ENV "/usr/share/python3/scirius"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
